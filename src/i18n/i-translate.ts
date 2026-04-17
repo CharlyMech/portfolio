44c9db94ea43539e0c528109b85746d0dad7aae2
CharlyMech <sanchezreciocarlos99@gmail.com>
2026-04-17 15:59:37 +0200

    feat: add nonTechExperience and certificates keys to Translations interface

--- src/i18n/i-translate.ts
+++ src/i18n/i-translate.ts
@@ -105,6 +105,8 @@ export interface Translations {
 		fullList: string;
 		entries: string;
 		current: string;
+		nonTechExperience: string;
+		certificates: string;
 	};
 	lang: { switchTo: string; en: string; es: string };
 }
